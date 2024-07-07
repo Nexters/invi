c0bd1fd7f089119e411bafe1f711063ecea185da
bepyan <bepyan@naver.com>
2024-07-08 01:12:43 +0900

    chore: pretendard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin="anonymous"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
