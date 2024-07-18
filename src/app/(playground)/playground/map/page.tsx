d871cc0b0980ac92491e6d1d4c27d29f4a160647
Choi siun <siun0331@gmail.com>
2024-07-18 23:56:33 +0900

    feat: kakao map type 선언 및 컴포넌트로 분리

--- src/app/(playground)/playground/map/page.tsx
+++ src/app/(playground)/playground/map/page.tsx
@@ -1,40 +1,10 @@
-"use client"
-
-import { useEffect, useRef } from "react";
-import Script from "next/script";
+import KakaoMap from "~/components/KakaoMap";
 
 export default function KaKaoMapPage() {
-  const mapRef = useRef<HTMLDivElement | null>(null);
 
-  const initializeMap = () => {
-    if (!mapRef.current) return;
-
-    window.kakao.maps.load(() => {
-      const mapOption = {
-        center: new window.kakao.maps.LatLng(37.566826, 126.9786567),
-        level: 3,
-      };
-      new window.kakao.maps.Map(mapRef.current, mapOption);
-    });
-  };
-
-  useEffect(() => {
-    if (window.kakao && window.kakao.maps) {
-      initializeMap();
-    }
-  }, []);
-
-  return (
-    <>
-      <Script
-        strategy="afterInteractive"
-        type="text/javascript"
-        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&autoload=false`}
-        onLoad={initializeMap}
-      />
-      <div className="w-full h-[50vh] sm:h-[60vh] md:h-[70vh] lg:h-[80vh]">
-        <div ref={mapRef} className={'w-full h-full'} ></div>
-      </div>
-    </>
+  return(
+    <div>
+      <KakaoMap height={"400px"} width={"100%"} latitude={37.566828} longitude={126.9786567} addCenterPin={true} />
+    </div>
   )
 }
---
+++ src/types/global/kakao.d.ts
@@ -0,0 +1,16 @@
+declare global {
+  interface Window {
+    kakao: {
+      maps: {
+        load: (callback: () => void) => void;
+        LatLng: new (lat: number, lng: number) => any;
+        Map: new (container: HTMLElement, options: any) => any;
+        Marker: new (options: any) => {
+          setMap: (map: any) => void;
+        };
+      };
+    };
+  }
+}
+
+export {};
