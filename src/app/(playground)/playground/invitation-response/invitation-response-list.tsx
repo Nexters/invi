4f94ca30f706f671fdc26e440ee1543fbd5cae23
Gukhee Jo <nb92092@navercorp.com>
2024-07-22 22:18:35 +0900

    feat: 참석여부 단건 조회

--- src/app/(playground)/playground/invitation-response/invitation-response-list.tsx
+++ src/app/(playground)/playground/invitation-response/invitation-response-list.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
 import {
   Card,
   CardContent,
@@ -24,21 +25,28 @@ export default function InvitationResponseList() {
   return (
     <div className="grid grid-cols-3 gap-2">
       {data.map((response) => (
-        <Card key={response.id}>
-          <CardHeader>
-            <CardTitle>{response.participant_name}</CardTitle>
-            <CardDescription>
-              {response.attendance ? "참석" : "불참"}
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <CardDescription>ID: {response.id}</CardDescription>
-            <CardDescription>사유: {response.reason || "없음"}</CardDescription>
-            <CardDescription>
-              생성일: {new Date(response.created_at).toLocaleString()}
-            </CardDescription>
-          </CardContent>
-        </Card>
+        <Link
+          href={`/playground/invitation-response/${response.id}`}
+          key={response.id}
+        >
+          <Card>
+            <CardHeader>
+              <CardTitle>{response.participant_name}</CardTitle>
+              <CardDescription>
+                {response.attendance ? "참석" : "불참"}
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <CardDescription>ID: {response.id}</CardDescription>
+              <CardDescription>
+                사유: {response.reason || "없음"}
+              </CardDescription>
+              <CardDescription>
+                생성일: {new Date(response.created_at).toLocaleString()}
+              </CardDescription>
+            </CardContent>
+          </Card>
+        </Link>
       ))}
     </div>
   );
--- src/lib/db/schema/invitation_response.query.ts
+++ src/lib/db/schema/invitation_response.query.ts
@@ -1,8 +1,20 @@
 "use server";
 
+import { eq } from "drizzle-orm";
 import { db } from "~/lib/db";
-import { invitationResponses } from "~/lib/db/schema/invitation_response";
+import {
+  invitationResponses,
+  type InvitationResponse,
+} from "~/lib/db/schema/invitation_response";
 
 export async function getAllInvitationResponses() {
   return db.select().from(invitationResponses);
+}
+
+export async function getInvitationResponseById(id: InvitationResponse["id"]) {
+  const responses = await db
+    .select()
+    .from(invitationResponses)
+    .where(eq(invitationResponses.id, id));
+  return responses[0];
 }
