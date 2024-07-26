2120ae7838301e200829e5247bc32bcb5bde4928
WooWan <woohobi@gmail.com>
2024-07-26 13:29:36 +0900

    add: Sonner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Toaster as Sonner } from "~/components/ui/sonner";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -21,7 +22,10 @@ export default function RootLayout({
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css"
         />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Sonner />
+      </body>
     </html>
   );
 }
