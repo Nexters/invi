450f813d7b14e5721043b001d55f0a079c2bf0ca
WooWan <woohobi@gmail.com>
2024-07-26 13:30:11 +0900

    update: pretendard 폰트 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import { Toaster as Sonner } from "~/components/ui/sonner";
 import "./globals.css";
+
+const pretendard = localFont({
+  src: "../../public/fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+});
 
 export const metadata: Metadata = {
   title: "인비",
@@ -13,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="kr" className={`${pretendard.variable}`}>
       <head>
         <link
           rel="stylesheet"
@@ -22,7 +30,7 @@ export default function RootLayout({
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css"
         />
       </head>
-      <body>
+      <body className={pretendard.className}>
         {children}
         <Sonner />
       </body>
