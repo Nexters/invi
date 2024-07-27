baf6750790725288297baff80207081178efb998
WooWan <woohobi@gmail.com>
2024-07-27 10:32:54 +0900

    update: /ut로 redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/ut",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
