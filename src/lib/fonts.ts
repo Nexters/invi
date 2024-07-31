7076d24f4543150037540c401037644276bce7f5
bepyan <bepyan@naver.com>
2024-07-31 18:20:29 +0900

    refactor: mv fonts to assets

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 
 export const pretendard = localFont({
-  src: "../../public/fonts/PretendardVariable.woff2",
+  src: "../assets/fonts/PretendardVariable.woff2",
   display: "swap",
   weight: "45 920",
   variable: "--font-pretendard",
