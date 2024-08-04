4a6d0da4da5fd60fa437de4fa9718836353ee7ae
bepyan <bepyan@naver.com>
2024-08-05 01:02:26 +0900

    teat: custom error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,21 +1,33 @@
-import Link from "next/link";
+"use client";
+
+import { useEffect } from "react";
 import { Button } from "~/components/ui/button";
 
-export default function NotFoundPage() {
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  // 에러가 발생한 컴포넌트를 다시 렌더링
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // 에러 추적을 위함
+    console.error(error);
+  }, [error]);
+
   return (
     <main className="mx-auto max-w-6xl px-5 py-8 pb-20 lg:p-14">
       <div className="align-stretch mx-auto flex h-full w-full max-w-3xl flex-col">
         <div className="h-[45%] py-12">
           <div className="mb-16">
             <h1 className="mb-2 text-2xl font-medium">
-              페이지를 찾지 못했어요.
+              오류가 발생되었습니다.
             </h1>
             <p className="mb-8 text-sm text-muted-foreground">
-              페이지 주소가 정확한지 확인해주세요.
+              잠시 후 다시 시도해주세요.
             </p>
-            <Button asChild>
-              <Link href="/">홈으로 가기</Link>
-            </Button>
+            <Button onClick={() => reset()}>다시 시도</Button>
           </div>
         </div>
         <div className="w-full border-t py-4">
