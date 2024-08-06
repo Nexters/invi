05d7576b87e7d2a72e08835eaed32453c09842e1
bepyan <bepyan@naver.com>
2024-08-06 20:00:55 +0900

    fix: build error

--- src/app/(playground)/pg/error/page.tsx
+++ src/app/(playground)/pg/error/page.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import { useEffect } from "react";
+import { ALink, AMain } from "~/app/(playground)/pg/inner-tools";
+
 export default function Page() {
-  throw new Error("This is a test error");
+  useEffect(() => {
+    throw new Error("This is an intentional error");
+  }, []);
+
+  return (
+    <AMain>
+      <ALink href="./">playground</ALink>
+      <div>
+        <h1>Test</h1>
+      </div>
+    </AMain>
+  );
 }
