934314840c871dceb3cbb4ecbbea673bee1b5237
Edward Kim <65283190+bepyan@users.noreply.github.com>
2024-08-09 23:37:39 +0900

    refactor: placeholder (#32)
    * refactor: placeholders
    
    * style: sort attribute

--- src/components/editor/sidebar-elements-tab.tsx
+++ src/components/editor/sidebar-elements-tab.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import ContainerPlaceholder from "~/components/editor/elements/container-placeholder";
-import MapPlaceholder from "~/components/editor/elements/map-placeholder";
-import TextPlaceholder from "~/components/editor/elements/text-placeholder";
-import TwoColumnsPlaceholder from "~/components/editor/elements/two-columns-placeholder";
+import {
+  ContainerPlaceholder,
+  MapPlaceholder,
+  TextPlaceholder,
+  TwoColumnsPlaceholder,
+} from "~/components/editor/placeholders";
 import type { EditorElementType } from "~/components/editor/type";
 import {
   Accordion,
@@ -26,27 +28,27 @@ export default function SidebarElementsTab(props: Props) {
     group: "layout" | "elements";
   }[] = [
     {
-      Component: <ContainerPlaceholder />,
-      label: "Container",
       id: "container",
+      label: "Container",
+      Component: <ContainerPlaceholder />,
       group: "layout",
     },
     {
-      Component: <TwoColumnsPlaceholder />,
-      label: "2 Columns",
       id: "2Col",
+      label: "2 Columns",
+      Component: <TwoColumnsPlaceholder />,
       group: "layout",
     },
     {
-      Component: <TextPlaceholder />,
-      label: "Text",
       id: "text",
+      label: "Text",
+      Component: <TextPlaceholder />,
       group: "elements",
     },
     {
-      Component: <MapPlaceholder />,
-      label: "map",
       id: "map",
+      label: "map",
+      Component: <MapPlaceholder />,
       group: "elements",
     },
   ];
