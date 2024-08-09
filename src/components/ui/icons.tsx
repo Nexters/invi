aad48a0bc1c799df0387f02fd9c6657abf101876
bepyan <bepyan@naver.com>
2024-08-10 00:22:07 +0900

    fix: svg icon attribute

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -8,8 +8,8 @@ export function GapIcon() {
     >
       <path
         fill="currentColor"
-        fill-opacity="1"
-        fill-rule="nonzero"
+        fillOpacity="1"
+        fillRule="nonzero"
         stroke="none"
         d="M11 13v-2H1v2H0v-3h12v3h-1zm1-10H0V0h1v2h10V0h1v3zM9 7V6H3v1h6z"
       ></path>
@@ -27,8 +27,8 @@ export function PaddingLeftRightIcon() {
     >
       <path
         fill="currentColor"
-        fill-opacity="1"
-        fill-rule="evenodd"
+        fillOpacity="1"
+        fillRule="evenodd"
         stroke="none"
         d="M0 12V0h1v12H0zm3-9h6v6H3V3zm1 1v4h4V4H4zm7 8V0h1v12h-1z"
       ></path>
@@ -46,8 +46,8 @@ export function PaddingTopBottomIcon() {
     >
       <path
         fill="currentColor"
-        fill-opacity="1"
-        fill-rule="evenodd"
+        fillOpacity="1"
+        fillRule="evenodd"
         stroke="none"
         d="M3 3h6v6H3V3zm1 1v4h4V4H4zm8 8H0v-1h12v1zm0-11H0V0h12v1z"
       ></path>
@@ -65,8 +65,8 @@ export function PaddingLeftIcon() {
     >
       <path
         fill="currentColor"
-        fill-opacity="1"
-        fill-rule="evenodd"
+        fillOpacity="1"
+        fillRule="evenodd"
         stroke="none"
         d="M0 12V0h1v12H0zm3-9h6v6H3V3zm1 1v4h4V4H4z"
       ></path>
@@ -84,8 +84,8 @@ export function PaddingRightIcon() {
     >
       <path
         fill="currentColor"
-        fill-opacity="1"
-        fill-rule="evenodd"
+        fillOpacity="1"
+        fillRule="evenodd"
         stroke="none"
         d="M12 0v12h-1V0h1zM3 3h6v6H3V3zm1 1v4h4V4H4z"
       ></path>
@@ -103,8 +103,8 @@ export function PaddingTopIcon() {
     >
       <path
         fill="currentColor"
-        fill-opacity="1"
-        fill-rule="evenodd"
+        fillOpacity="1"
+        fillRule="evenodd"
         stroke="none"
         d="M0 0h12v1H0V0zm3 3h6v6H3V3zm1 1v4h4V4H4z"
       ></path>
@@ -122,8 +122,8 @@ export function PaddingBottomIcon() {
     >
       <path
         fill="currentColor"
-        fill-opacity="1"
-        fill-rule="evenodd"
+        fillOpacity="1"
+        fillRule="evenodd"
         stroke="none"
         d="M3 3h6v6H3V3zm1 1v4h4V4H4zm8 8H0v-1h12v1z"
       ></path>
@@ -136,8 +136,8 @@ export function PaddingIndividualIcon() {
     <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12">
       <path
         fill="currentColor"
-        fill-opacity=".8"
-        fill-rule="evenodd"
+        fillOpacity=".8"
+        fillRule="evenodd"
         stroke="none"
         d="M3 0h6v1H3V0zM0 3v6h1V3H0zm11 0v6h1V3h-1zm-8 9h6v-1H3v1z"
       ></path>
