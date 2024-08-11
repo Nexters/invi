11e590df985b3d35aa1f07e26b18244604da518e
Gibeom Lim <delphox60@gmail.com>
2024-08-11 14:19:43 +0900

    feat: implement modify invitation method

---
+++ src/lib/db/schema/invitations.query.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { db } from "~/lib/db";
+import { invitations } from "~/lib/db/schema/invitations";
+
+type UpdateInvitationParams = {
+  id: string;
+  title?: string;
+  description?: string;
+  eventDate?: Date;
+  eventUrl?: string;
+  customFields?: Record<string, any>;
+};
+
+async function updateInvitation(params: UpdateInvitationParams) {
+  const { id, ...updates } = params;
+
+  if (!id) {
+    throw new Error("ID is required to update an invitation");
+  }
+
+  try {
+    await db.update(invitations).set(updates).where(invitations.id.eq(id));
+  } catch (error) {
+    console.error("Error updating invitation:", error);
+    throw new Error("Could not update invitation");
+  }
+}
