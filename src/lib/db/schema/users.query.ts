36b86e01704af844beb19960beec77ec86acc86d
Gibeom Lim <delphox60@gmail.com>
2024-08-11 15:33:09 +0900

    feat: 유저 삭제 (회원 탈퇴) 메서드 구현

--- src/lib/db/schema/users.query.ts
+++ src/lib/db/schema/users.query.ts
@@ -32,3 +32,22 @@ export async function findOrCreateUser(user: Omit<UserInsert, "id">) {
 
   return dbUser;
 }
+
+export async function deleteUser(id: string): Promise<void> {
+  if (!id) {
+    throw new Error("ID is required to delete a user");
+  }
+
+  try {
+    const result = await db.delete(users).where(eq(users.id, id));
+
+    // TODO cascade delete template, invitaions, etc
+
+    if (result.rowCount === 0) {
+      throw new Error("User not found");
+    }
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    throw new Error("Could not delete user");
+  }
+}
