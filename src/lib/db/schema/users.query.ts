7e41990c21ffaa4b9cfa77b852f3691a11affe2b
Gibeom Lim <delphox60@gmail.com>
2024-08-14 23:26:11 +0900

    Feature/inv-108 회원 탈퇴 및 로그아웃 구현 (#39)
    * feat: 유저 삭제 (회원 탈퇴) 메서드 구현
    
    * feat: 유저 로그아웃 메서드 구현

--- src/lib/db/schema/users.query.ts
+++ src/lib/db/schema/users.query.ts
@@ -3,6 +3,7 @@
 import { eq } from "drizzle-orm";
 import { generateId } from "lucia";
 import { db } from "~/lib/db";
+import { sessions } from "~/lib/db/schema/auth";
 import { users, type UserInsert } from "~/lib/db/schema/users";
 
 export async function createUser(data: Omit<UserInsert, "id">) {
@@ -31,4 +32,33 @@ export async function findOrCreateUser(user: Omit<UserInsert, "id">) {
   }
 
   return dbUser;
+}
+
+export async function deleteUser(id: string): Promise<void> {
+  if (!id) {
+    throw new Error("ID is required to delete a user");
+  }
+
+  try {
+    const result = await db.delete(users).where(eq(users.id, id));
+
+    // TODO cascade delete template, invitaions, etc
+
+    if (result.rowCount === 0) {
+      throw new Error("User not found");
+    }
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    throw new Error("Could not delete user");
+  }
+}
+
+export async function logout(userId: string) {
+  try {
+    db.delete(sessions).where(eq(sessions.userId, userId));
+    console.log(`User ${userId} logged out successfully.`);
+  } catch (error) {
+    console.error("Error logging out user:", error);
+    throw new Error("Could not log out user");
+  }
 }
