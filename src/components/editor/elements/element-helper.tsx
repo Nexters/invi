eed3db0fe2f741dc711f080161f51cbc9783ba18
bepyan <bepyan@naver.com>
2024-08-15 20:32:49 +0900

    fix: build

--- src/components/editor/elements/element-helper.tsx
+++ src/components/editor/elements/element-helper.tsx
@@ -37,7 +37,7 @@ export default function ElementHelper() {
   };
 
   return (
-    document.body &&
+    typeof window !== "undefined" &&
     createPortal(
       !editor.state.isPreviewMode && elementRef?.current && (
         <div
