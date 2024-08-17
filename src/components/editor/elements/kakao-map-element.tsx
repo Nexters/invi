98a5de00218a22494d29b0751562e0d629008ee4
bepyan <bepyan@naver.com>
2024-08-17 22:31:07 +0900

    refactor: cleanup unused

--- src/components/editor/elements/kakao-map-element.tsx
+++ src/components/editor/elements/kakao-map-element.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useEditor } from "~/components/editor/provider";
 import type { InferEditorElement } from "~/components/editor/type";
 
@@ -18,8 +18,8 @@ export default function KakaoMapElement({
   latitude,
   longitude,
 }: Props) {
-  const { dispatch, editor } = useEditor();
-  const isSelected = editor.state.selectedElement.id === element.id;
+  const { editor } = useEditor();
+
   const mapRef = useRef<HTMLDivElement | null>(null);
   const mapInstanceRef = useRef<any>(null);
   const markerRef = useRef<any>(null);
@@ -70,26 +70,9 @@ export default function KakaoMapElement({
     }
   }, [editor.state.selectedElement]);
 
-  const handleDeleteElement = () => {
-    dispatch({
-      type: "DELETE_ELEMENT",
-      payload: { elementDetails: element },
-    });
-  };
-
-  const handleOnClickBody = (e: React.MouseEvent) => {
-    e.stopPropagation();
-    dispatch({
-      type: "CHANGE_CLICKED_ELEMENT",
-      payload: {
-        elementDetails: element,
-      },
-    });
-  };
-  //WE ARE NOT ADDING DRAG DROP
   return (
     <div className={"relative h-[250px] w-full"}>
-      <div style={{ height: "250px" }} onClick={handleOnClickBody}>
+      <div style={{ height: "250px" }}>
         <div ref={mapRef} style={{ width: "100%", height: "100%" }} />
       </div>
     </div>
