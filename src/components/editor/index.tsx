d77f8394efe43b26d19407e48be9500b1cd58b79
bepyan <bepyan@naver.com>
2024-08-18 03:55:51 +0900

    cleanup

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -6,7 +6,7 @@ import EditorSidebar from "~/components/editor/sidebar";
 export default function Editor(props: EditorProps) {
   return (
     <EditorProvider {...props}>
-      <div className="fixed bottom-0 left-0 right-0 top-0 z-[20] flex flex-col overflow-hidden bg-secondary">
+      <div className="fixed bottom-0 left-0 right-0 top-0 z-20 flex flex-col overflow-hidden bg-secondary">
         <EditorNavigation />
         <EditorMain />
         <EditorSidebar />
