0a854b6adebd7a1a6c66ed1436a2eb711cd8adf7
Edward Kim <65283190+bepyan@users.noreply.github.com>
2024-08-21 03:09:02 +0900

    feat: 로딩 (#80)
    * chore: InView with framer-motion
    
    * feat: add loading layout
    
    * feat: text-effect
    
    * feat: global loading
    
    * feat: delay create invitation
    
    * feat: 저장 중 로딩
    
    * feat: 삭제 토스트 추가
    
    * feat: 저장 버튼 평소 활성화
    
    * feat: OG 이미지 설정 (#79)
    
    * feat: update thumbnail
    
    * feat: apply og image
    
    * feat: add layout thumbnail
    
    * feat: 템플릿의 썸네일이 초기 적용되도록

---
+++ src/components/core/in-view.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import {
+  motion,
+  type Transition,
+  useInView,
+  type UseInViewOptions,
+  type Variant,
+} from "framer-motion";
+import { type ReactNode, useRef } from "react";
+
+interface InViewProps {
+  children: ReactNode;
+  variants?: {
+    hidden: Variant;
+    visible: Variant;
+  };
+  transition?: Transition;
+  viewOptions?: UseInViewOptions;
+  className?: string;
+}
+
+const defaultVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
+
+export function InView({
+  children,
+  variants = defaultVariants,
+  transition,
+  viewOptions,
+  className,
+}: InViewProps) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, viewOptions);
+
+  return (
+    <motion.div
+      ref={ref}
+      initial="hidden"
+      animate={isInView ? "visible" : "hidden"}
+      variants={variants}
+      transition={transition}
+      className={className}
+    >
+      {children}
+    </motion.div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,11 +75,35 @@ const config = {
             transform: "scale(1)",
           },
         },
+        "head-shake": {
+          "0%": {
+            transform: "translateX(0)",
+          },
+          "6.5%": {
+            transform: "translateX(-6px) rotateY(-9deg)",
+          },
+
+          "18.5%": {
+            transform: "translateX(5px) rotateY(7deg)",
+          },
+
+          "31.5%": {
+            transform: "translateX(-3px) rotateY(-5deg)",
+          },
+
+          "43.5%": {
+            transform: "translateX(2px) rotateY(3deg)",
+          },
+          "50%": {
+            transform: "translateX(0)",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "zoom-in": "zoom-in 0.5s ease-out",
+        "head-shake": "head-shake 2s infinite",
       },
     },
   },
