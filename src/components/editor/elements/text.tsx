3ccebf8e405b486494259288247a5009824e10bb
bepyan <bepyan@naver.com>
2024-08-22 01:04:53 +0900

    chore: sanitize-html

--- src/components/editor/elements/text.tsx
+++ src/components/editor/elements/text.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { useRef } from "react";
+import { useCallback, useRef } from "react";
 import ContentEditable, {
   type ContentEditableEvent,
 } from "react-contenteditable";
+import sanitizeHtml from "sanitize-html";
 import ElementWrapper from "~/components/editor/elements/element-wrapper";
 import { useEditor } from "~/components/editor/provider";
 import type { InferEditorElement } from "~/components/editor/type";
@@ -15,20 +16,29 @@ type Props = {
 export default function Text({ element }: Props) {
   const { dispatch, editor } = useEditor();
 
-  const text = useRef(element.content.innerText);
+  const sanitize = useCallback((html: string) => {
+    return sanitizeHtml(html, {
+      allowedTags: ["a", "p", "br", "div", "b"],
+      allowedAttributes: { a: ["href"], p: ["style"] },
+    });
+  }, []);
+
+  const text = useRef(sanitize(element.content.innerText));
 
   const handleChange = (e: ContentEditableEvent) => {
     text.current = e.target.value;
   };
 
   const handleBlur = () => {
+    const textHtml = sanitize(text.current);
+    text.current = textHtml;
     dispatch({
       type: "UPDATE_ELEMENT",
       payload: {
         elementDetails: {
           ...element,
           content: {
-            innerText: text.current,
+            innerText: textHtml,
           },
         },
       },
@@ -39,7 +49,7 @@ export default function Text({ element }: Props) {
     <ElementWrapper element={element}>
       <ContentEditable
         className="w-full outline-none"
-        disabled={!editor.state.isPreviewMode}
+        disabled={editor.state.isPreviewMode}
         html={text.current}
         onBlur={handleBlur}
         onChange={handleChange}
