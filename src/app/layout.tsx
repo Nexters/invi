6326ca374cf2815602edd1de1d47cd15ba7086b3
bepyan <bepyan@naver.com>
2024-08-22 02:46:56 +0900

    feat: SEO 설명 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "초대장 플랫폼, 인비",
-  description: "따뜻한 마음을 담아 당신의 환대를 전해보세요.",
+  description: "따뜻한 마음이 담긴 당신의 환대",
   openGraph: {
     images:
       "http://t1.daumcdn.net/brunch/service/user/d4v5/image/Axc3mTi7LoZC2GpsBWosDpRrNPU.png",
