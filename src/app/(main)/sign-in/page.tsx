8c3529468995f6f282ad44a30b1d024c7711ee27
WooWan <woohobi@gmail.com>
2024-08-23 23:26:42 +0900

    fix: 로그인 qa 반영

--- src/app/(main)/sign-in/page.tsx
+++ src/app/(main)/sign-in/page.tsx
@@ -16,64 +16,71 @@ export default async function Page() {
   }
 
   return (
-    <main className="mx-auto flex min-h-screen w-fit flex-col items-center justify-center">
-      <div className="relative">
-        <Image
-          src={Bee}
-          alt="bee"
-          width={125}
-          height={111}
-          className="absolute -right-[30px] -top-[90px]"
-        />
-        <Image
-          src={Star}
-          alt="star"
-          width={63}
-          height={56}
-          className="absolute -left-[80px] top-[100px]"
-        />
-        <Image
-          src={Mail}
-          alt="mail"
-          width={100}
-          height={100}
-          className="absolute -right-[100px] top-[270px]"
-        />
-        <section className="mb-[56px] flex flex-col items-center gap-y-6">
-          <h2 className="text-[2.5em] font-semibold -tracking-[0.2px]">
-            당신의 환대, INVI
-          </h2>
-          <p className="text-xl -tracking-[0.2px] text-[#333333]">
-            인비와 함께 나만의 초대장을 만들어보세요
-          </p>
-        </section>
-        <section className="flex min-w-[417px] flex-col items-center gap-y-7">
-          <Button variant="outline" className="h-16 w-full" asChild>
-            <Link
-              href="/sign-in/google"
-              className="flex gap-x-2 rounded-md px-6 py-4"
-            >
-              <Image
-                src={GoogleLogo}
-                width={38}
-                height={38}
-                alt="google logo"
-                className="rounded-full"
-              />
-              Google 로그인
-            </Link>
-          </Button>
-          <span className="flex gap-x-2 text-gray-800">
-            <p className="text-gray-800">아직 계정이 없으신가요?</p>
-            <Link
-              href="/sign-in/google"
-              className="font-semibold text-gray-950 underline underline-offset-2"
-            >
-              가입하기
-            </Link>
-          </span>
-        </section>
-      </div>
-    </main>
+    <>
+      <header className="flex items-center pl-10 pt-8">
+        <Link href="/">
+          <Image src="/landing-logo.png" alt="logo" width={85} height={29} />
+        </Link>
+      </header>
+      <main className="mx-auto flex min-h-screen w-fit flex-col items-center justify-center">
+        <div className="relative">
+          <Image
+            src={Bee}
+            alt="bee"
+            width={125}
+            height={111}
+            className="absolute -right-[30px] -top-[90px]"
+          />
+          <Image
+            src={Star}
+            alt="star"
+            width={63}
+            height={56}
+            className="absolute -left-[80px] top-[100px]"
+          />
+          <Image
+            src={Mail}
+            alt="mail"
+            width={100}
+            height={100}
+            className="absolute -right-[100px] top-[270px]"
+          />
+          <section className="mb-[56px] flex flex-col items-center gap-y-6">
+            <h2 className="text-[2.5em] font-semibold -tracking-[0.2px]">
+              당신의 환대, INVI
+            </h2>
+            <p className="text-xl -tracking-[0.2px] text-[#333333]">
+              인비와 함께 나만의 초대장을 만들어보세요
+            </p>
+          </section>
+          <section className="flex min-w-[417px] flex-col items-center gap-y-7">
+            <Button variant="outline" className="h-16 w-full" asChild>
+              <Link
+                href="/sign-in/google"
+                className="flex gap-x-2 rounded-md px-6 py-4"
+              >
+                <Image
+                  src={GoogleLogo}
+                  width={38}
+                  height={38}
+                  alt="google logo"
+                  className="rounded-full"
+                />
+                <p className="text-xl">Google 로그인</p>
+              </Link>
+            </Button>
+            <span className="flex gap-x-2 text-gray-800">
+              <p className="text-gray-800">아직 계정이 없으신가요?</p>
+              <Link
+                href="/sign-in/google"
+                className="font-semibold text-gray-950 underline underline-offset-2"
+              >
+                가입하기
+              </Link>
+            </span>
+          </section>
+        </div>
+      </main>
+    </>
   );
 }
